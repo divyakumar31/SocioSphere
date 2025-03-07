7b99bde752083c4b34353a4f86f6015555261256
divyakumar patel <d.b.patel691@gmail.com>
2025-03-07 11:14:54 +0530

    Initial Setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Hello World</Button>
+      <h1 className="text-3xl font-bold text-blue-500">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
